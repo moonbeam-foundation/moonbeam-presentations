7b79b8d5a4b855f272a299a81f62c0c26fff294e
albertov19 <albertoviera@gmail.com>
2021-03-03 10:50:36 +0000

    Sub0 2020-10 Presentation

--- 202012-encoded-club/CrowdCoin/ethereum/campaign.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import Campaign from './build/Campaign.json';
-
-// Create local instance of Campaign
-const campaignInstance = (campaignAddress) => {
-   return new web3.eth.Contract(Campaign.abi, campaignAddress);
-};
-
-export default campaignInstance;
